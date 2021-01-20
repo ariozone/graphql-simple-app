c7aab74da6d577dbc48c4b657ca69dd98039a9e4
Ario Tamadoni <ariozone6@gmail.com>
2021-01-19 17:04:52 -0800

    Server create that uses graphql.

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 const express = require('express')
+// const LexpressGraphQ = require('express-graphql')
+const { graphqlHTTP } = require('express-graphql')
+const schema = require('./schema/schema');
+
 const app = express()
-app.listen(4000, () => console.log('listening...'))
+
+app.use('/graphql', graphqlHTTP({
+  schema: {
+
+  },
+  graphiql: true
+}))
+
+app.listen(4000, () => {
+  console.log('Listening...');
+})
