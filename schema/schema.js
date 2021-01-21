2ae7a51950271ce14f5a558fff006c78f1d5790b
Ario Tamadoni <ariozone6@gmail.com>
2021-01-20 16:35:53 -0800

    Create User schema.

--- schema/schema.js
+++ schema/schema.js
@@ -1,1 +1,15 @@
-//schema here
+const graphpl = require('graphql')
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt
+} = graphpl
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  }
+})
