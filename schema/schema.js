eda9b10c8246cb3c90730a861bef8b7c572fae20
Ario Tamadoni <ariozone6@gmail.com>
2021-01-25 18:05:39 -0800

    Associate company with user.

--- schema/schema.js
+++ schema/schema.js
@@ -8,14 +8,32 @@ const {
 } = graphpl
 
 
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+
+})
+
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost: 3000/companies/${parentValue.companyId}`)
+          .then(res => res.data)
+      }
+    }
   }
 })
+
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -25,7 +43,7 @@ const RootQuery = new GraphQLObjectType({
       args: {
         id: { type: GraphQLString }
       },
-      resolve: (parentValue, args) => {
+      resolve(parentValue, args) {
         return axios.get(`http://localhost:3000/users/${args.id}`)
           .then(respond => respond.data)
       }
